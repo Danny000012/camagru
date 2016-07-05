5b7eaa79bf79e9015072286a61c97aedcbbd75d2
Kiefer Wiessler <Kiefer@MacBook-Pro-Kiefer.local>
2016-07-05 17:29:49 +0200

    initial and dirty work but it's working

---
+++ menu_resp/myproject/js/tools.js
@@ -0,0 +1,15 @@
+function reset() {
+	var elem = document.body;
+
+	if (elem.className.match(/(?:^|\s)width--sidebar(?!\S)/))
+		elem.className = '';
+}
+
+function hide() {
+	var body = document.body;
+
+	if (body.className.match(/(?:^|\s)width--sidebar(?!\S)/))
+		body.className = '';
+	else
+		body.className = 'width--sidebar';
+}
