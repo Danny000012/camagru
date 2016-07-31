7037692227420d56d89d4822fc400a691566a26c
Victor <vquesnel@student.42.fr>
2016-07-31 13:12:17 +0200

    modify css for sign in & reset passwd center all input

--- reset_password.php
+++ reset_password.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf8">
     <link href="stylesheets/sign_in.css" rel="stylesheet">
-        <link href="stylesheets/menu.css" rel="stylesheet">
+    <link href="stylesheets/menu.css" rel="stylesheet">
 
     <link href="stylesheets/reset_password.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
@@ -15,36 +15,33 @@
     <div class="site-content">
         <div class="site-cache" id="site-cache" onClick="hide()">
             <div class="container" align="center" onClick="hide()">
-                
+
                 <div class="form-content" onClick="hide()">
                     <img text-align="center" src="img/locked.png" alt="user_logo" class="img_form">
                     <div class="title" align="center">Modify password</div>
-                    <form class="form" method="get" action="">
+                    <form align="center" class="form" method="get" action="">
                         <div class="item">Login</div>
-                        <input type="text" name="login">
+                        <input style="text-align:center;" class="input" type="text" name="login">
                         <br>
                         <div class="item">Old password</div>
-                        <input type="password" name="oldpassword">
+                        <input style="text-align:center;" class="input" type="password" name="oldpassword">
                         <br>
                         <div class="item">New passord</div>
-                        <input type="password" name="newpassword">
+                        <input style="text-align:center;" class="input" type="password" name="newpassword">
                         <br>
                         <div class="item">Confimation</div>
-                        <input type="password" name="confirmnewpassword">
+                        <input style="text-align:center;" class="input" type="password" name="confirmnewpassword">
                         <br>
                         <div align="center">
-                            <div>
-                                <input type="submit" name="button" value="Change password" class="button">
-                            </div>
-
-
+                            <input type="submit" name="button" value="Change password" class="button">
+                            <br>
                             <a href="forgot_password.php" class="fdp">Forgot your password ?</a>
                         </div>
                     </form>
-                   
+
                 </div>
                 <div class="separator"></div>
-                 <a href="feed.php" class="back">Back</a>
+                <a href="feed.php" class="back">Back</a>
                 <div class="separator"></div>
             </div>
         </div>
