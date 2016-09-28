7ac32dd74c064d0bbfc35f8e3c37a153bd345adb
Kiefer WIESSLER <kwiessle@e2r4p3.42.fr>
2016-09-28 14:39:12 +0200

    clean

--- post.php
+++ post.php
@@ -85,15 +85,6 @@
                                 </form>
                             </div>
                             <div class="hud">
-<<<<<<< HEAD
-                                <video autoplay="true" id="videoElement">
-
-                                </video>
-                                <div class="menu-cam">
-                                    <a href="#" type="button"><img src="img/cam.png" alt="camera>" class="img-logo"></a>
-                                </div>
-                                <div class="upload">
-=======
 								<video id="video"></video>
 								<canvas id="canvas"></canvas>
 								<div class="menu-cam">
@@ -101,9 +92,7 @@
 									<a href="post.php" type="button"><img src="img/garbage.png" alt="camera" class="img-logo"></a>
 									<a id="cancel montage" type="button"><img src="img/flag.jpg" alt="camera" class="img-logo"></a>
 								</div>
-							</div>
 								<div class="upload">
->>>>>>> 17141269ac4ce7861a452bde5d62e0cb105b392e
                                     <h2>Upload an image</h2>
                                     <form method="post" enctype="multipart/form-data" action="#" class="upload-form">
                                         <input type="file" name="image" />
